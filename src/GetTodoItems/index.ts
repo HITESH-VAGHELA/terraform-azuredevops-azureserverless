b8408e22a6ac7e72ccce145e2261222f6b69e141
Hitesh <hitesh@123.com>
2023-02-12 14:57:43 +0530

    imports removed

--- src/GetTodoItems/index.ts
+++ src/GetTodoItems/index.ts
@@ -1,9 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { getUserId } from "../Common/Utils";
 import { getAllTodoItems } from "../DataAccess/todo-item-repository";
-import { NewTodoItem } from "../Models/new-todo-item";
-import { TodoItem } from "../Models/todo-item";
-import { ToDoItemRecord } from "../Models/todo-item-record";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log.info('Get all todo items started.');
