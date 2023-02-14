8c374a228d799f69803bc0925e44a54bafee10c0
Hitesh <hitesh@123.com>
2023-02-14 11:48:44 +0530

    scripts folder added

---
+++ devops/scripts/terraformworkspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "****Create or Select Workspace****"
+if [ (terraform workspace list | grep -c "$1") -eq 0 ]; then
+  echo "Create new workspace $1"
+  terraform workspace new "$1"
+else
+  echo "Switch to workspace $1"
+  terraform workspace select "$1"
+fi
