4039d29a101dda9a01b5cda8cdefe5a2246c9cd4
Hitesh <hitesh@123.com>
2023-02-16 12:26:34 +0530

    Azure Function App module Commented

--- devops/infra/outputs.tf
+++ devops/infra/outputs.tf
@@ -1,9 +1,9 @@
-output "azurerm_function_app_host_key" {
-  value     = module.terraform-module-function-app.azurerm_function_app_host_key
-  sensitive = true
-}
-
-output "azurerm_function_name_out" {
-  value = module.terraform-module-function-app.azurerm_function_name_out
-}
+#output "azurerm_function_app_host_key" {
+#  value     = module.terraform-module-function-app.azurerm_function_app_host_key
+#  sensitive = true
+#}
+#
+#output "azurerm_function_name_out" {
+#  value = module.terraform-module-function-app.azurerm_function_name_out
+#}
 
